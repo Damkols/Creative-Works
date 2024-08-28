caab965b9baa490161897af3844a650363adbbe3
Damkols <damolausmankolapo@gmail.com>
2024-08-28 20:15:05 +0100

    cascading realties

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Slider from "@/components/slider/Slider";
+
+export default function Home() {
+  return (
+    <>
+      <Slider />
+    </>
+  );
+}
---
+++ src/components/slider/videos.js
@@ -0,0 +1,26 @@
+export const videos = [
+  {
+    id: "1003752276",
+    title: "Grace Ruthina",
+    category: "Face",
+    date: "August 2024",
+  },
+  {
+    id: "1003747268",
+    title: "Yosemite",
+    category: "Face",
+    date: "September 2024",
+  },
+  {
+    id: "1003745007",
+    title: "Orihima",
+    category: "Face",
+    date: "May 2024",
+  },
+  {
+    id: "1003744467",
+    title: "Gbwagbalada",
+    category: "Face",
+    date: "June 2024",
+  },
+];
